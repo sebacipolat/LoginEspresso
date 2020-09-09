75da76ca2940f55dcdf890c113c04485a4a29bc0
sebastian cipolat <sebastiancipolat@gmail.com>
2020-09-08 23:10:17 -0300

    refactor to mvvm

--- app/src/main/java/com/cipolat/loginespresso/ui/login/LoginActivity.kt
+++ app/src/main/java/com/cipolat/loginespresso/ui/login/LoginActivity.kt
@@ -3,14 +3,19 @@ package com.cipolat.loginespresso.ui.login
 import android.os.Bundle
 import android.view.View.OnFocusChangeListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.cipolat.loginespresso.R
 import com.cipolat.loginespresso.ui.dialog.DialogFactory
+import com.cipolat.loginespresso.ui.extension.clear
+import com.cipolat.loginespresso.ui.extension.getEmojiByUnicode
 import com.cipolat.loginespresso.ui.extension.hideKeyboard
+import com.cipolat.loginespresso.ui.login.Emojis.ERROR_EMOJI
+import com.cipolat.loginespresso.ui.login.Emojis.PULGAR_EMOJI
 import com.cipolat.loginespresso.ui.login.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     lateinit var loginViewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,47 +29,50 @@ class MainActivity : AppCompatActivity() {
                 validateEmailInput(inputUser.text.toString())
             }
         }
+        listeLoginStatus()
         loginBtn.setOnClickListener { signIn() }
     }
 
+    private fun listeLoginStatus(){
+        val loginObserver = Observer<Boolean> {
+            if (it) {
+                showOkMessage()
+            } else {
+                showErrorMessage()
+            }
+        }
+        loginViewModel.loginData.observe(this, loginObserver)
+    }
 
     private fun signIn() {
         this.hideKeyboard()
         val validEmail = validateEmailInput(inputUser.text.toString())
         val validPassword = validatePasswInput(inputPassw.text.toString())
         if (validEmail && validPassword) {
-            if (!checkLoginData(inputUser!!.text.toString(), inputPassw.text.toString())) {
-                showErrorMessage()
-                cleanInputs()
-            } else showOkMessage()
+            loginViewModel.makeLogin(inputUser!!.text.toString(), inputPassw.text.toString())
         }
+
+
+
     }
 
-    private fun getEmojiByUnicode(unicode: Int): String {
-        return String(Character.toChars(unicode))
-    }
 
     private fun showErrorMessage() {
-        val unicode = 0x270B
         val message = """
             ${getString(R.string.invalidCredentials_message)}
-            ${getEmojiByUnicode(unicode)}
+            ${ERROR_EMOJI.getEmojiByUnicode()}
             """.trimIndent()
         DialogFactory.showInfoDialog(getString(R.string.invalidCredentials_title), message, this)
+        inputUser.clear()
+        inputPassw.clear()
     }
 
     private fun showOkMessage() {
-        val unicode = 0x1F44D
         val message = """
             ${getString(R.string.validCredentials_message)}
-            ${getEmojiByUnicode(unicode)}
+            ${PULGAR_EMOJI.getEmojiByUnicode()}
             """.trimIndent()
         DialogFactory.showInfoDialog(getString(R.string.validCredentials_title), message, this)
-    }
-
-    private fun cleanInputs() {
-        inputUser.setText("")
-        inputPassw.setText("")
     }
 
     private fun validateEmailInput(email: String): Boolean {
@@ -81,7 +89,4 @@ class MainActivity : AppCompatActivity() {
         return status
     }
 
-    private fun checkLoginData(username: String, passw: String): Boolean {
-        return username == "sebastiancipolat@gmail.com" && passw == "android2018"
-    }
 }
--- app/src/main/java/com/cipolat/loginespresso/ui/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/cipolat/loginespresso/ui/login/viewmodel/LoginViewModel.kt
@@ -2,13 +2,15 @@ package com.cipolat.loginespresso.ui.login.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.cipolat.loginespresso.ui.login.auth.AuthValidation
 
 class LoginViewModel : ViewModel() {
 
     var loginData: MutableLiveData<Boolean> = MutableLiveData()
 
     fun makeLogin(userName:String, password:String){
-
+        val auth= AuthValidation()
+        loginData.postValue(auth.checkLoginData(userName,password))
     }
 
 }
